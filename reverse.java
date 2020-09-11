cd90b088d1b7d0ef3a6a1198c6b2dde2473b5030
willdonner <willdonner4@gmail.com>
2020-09-11 15:43:52 +0800

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -4,6 +4,13 @@ class Solution {
         while(x!=0){
             int pop = x % 10;
             x /= 10;
+            if(rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > Integer.MAX_VALUE % 10)){
+                rev = 0;
+                break;
+            }else if(rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && x < Integer.MIN_VALUE % 10)){
+                rev = 0;
+                break;
+            }
             rev = rev *10 +pop;
         }
         return rev;
